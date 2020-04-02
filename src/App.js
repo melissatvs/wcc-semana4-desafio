4771eb72ad40caae8960894f0a49dac51df1a5e1
melissatvs <melissa.tv@hotmail.com>
2020-04-01 22:47:35 -0300

    add : iniciando repositório

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Usuarios from './Componentes/Usuarios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+        <main>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Telefone</th>
+                        <th>Álbuns</th>
+                        <th>Posts</th>
+                        <th>Fotos</th>
+                    </tr>
+                </thead>
+                <Usuarios />
+            </table>
+        </main>
+        );
+    }
+    
+    export default App;
+    
